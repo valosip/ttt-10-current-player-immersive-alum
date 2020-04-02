e1b1f909db86940809bdb044e0cf2703a192d7a9
Val Osip <valosip@gmail.com>
2020-04-02 23:23:39 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,12 @@
-def turn_count
+def turn_count(board)
+  count = 0
+  board.each do |value| 
+    if value == "X" || value == "O" 
+      count += 1
+  end
+  return count
 end
 
 def current_player
+  
 end
